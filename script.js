6a98903868fd0e012733647fa8f2a315ad65af31
Gabriela Chiquetto <gabrielachiquetto@gmail.com>
2022-06-13 19:16:20 -0300

    project finished

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 'use strict';
 const box = document.querySelector('.pictures-container');
+const boxes = document.querySelectorAll('.pictures-container__box');
 
-box.addEventListener('click', e => console.log(e.target.closest('div').classList));
+box.addEventListener('click', 
+e => {
+    if (e.target !== box){
+        const boxPicture = e.target.closest('div');
+        boxes.forEach(b => b.classList.remove('pictures-container__box--active'));
+        boxPicture.classList.add('pictures-container__box--active');
+    }    
+}
+);
 
